87978d0072e6771511cecf8684c3c08ae73f6b40
Joshua360 <joshuaowuor27@gmail.com>
2021-11-11 10:38:20 +0300

    port made dynamic

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ app.post("/profile", (req,res)=>{
 
 
 
-
-app.listen(3000, ()=>{
-    console.log("app is running on port:3000");
+const port = process.env.PORT || 3000;
+app.listen(port, ()=>{
+    console.log("app is running on port: " + port);
 });
